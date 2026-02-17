b76d7f37464e49d7b2dedb031017e51c5bae8a55
gauchoms <ricardo.feldens@gmail.com>
2026-02-17 14:14:02 -0300

    tela de projeções funcionando

--- app/models.py
+++ app/models.py
@@ -21,10 +21,12 @@ class Projecao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     jogo_id = db.Column(db.Integer, db.ForeignKey('jogo.id'), nullable=False)
     time_id = db.Column(db.Integer, db.ForeignKey('time.id'), nullable=False)
-    pontos_projetados = db.Column(db.Integer, nullable=True)  # 0, 1 ou 3
+    tipo = db.Column(db.String(20), nullable=False)  # 'titulo', 'libertadores', 'rebaixamento'
+    pontos = db.Column(db.Integer, nullable=True)    # 0, 1 ou 3
 
 class Meta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     time_id = db.Column(db.Integer, db.ForeignKey('time.id'), nullable=False)
     descricao = db.Column(db.String(50), nullable=False)  # 'titulo', 'libertadores', 'rebaixamento'
     pontos_alvo = db.Column(db.Integer, nullable=False)   # 68, 70 ou 45
+
