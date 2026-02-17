d474521cabc1e9b4a172623f1267c9c263f5a175
gauchoms <ricardo.feldens@gmail.com>
2026-02-17 17:51:20 -0300

    rota temporária ppara setup inicial no render

--- app/routes.py
+++ app/routes.py
@@ -270,3 +270,52 @@ def dashboard():
         detalhe=detalhe,
         ordenar_por=ordenar_por
     )
+
+@bp.route('/setup_inicial_render', methods=['GET'])
+def setup_inicial_render():
+    from app.api import get_jogos_brasileirao, processar_jogos
+    
+    # Verifica se já tem dados
+    if Time.query.count() > 0:
+        return jsonify({'mensagem': 'Banco já possui dados'})
+    
+    data = get_jogos_brasileirao()
+    jogos = processar_jogos(data)
+    
+    times_cadastrados = {}
+    
+    for jogo in jogos:
+        # Cadastra time da casa
+        if jogo['time_casa_id'] not in times_cadastrados:
+            time = Time.query.filter_by(api_id=jogo['time_casa_id']).first()
+            if not time:
+                time = Time(api_id=jogo['time_casa_id'], nome=jogo['time_casa'])
+                db.session.add(time)
+                db.session.flush()
+            times_cadastrados[jogo['time_casa_id']] = time.id
+        
+        # Cadastra time de fora
+        if jogo['time_fora_id'] not in times_cadastrados:
+            time = Time.query.filter_by(api_id=jogo['time_fora_id']).first()
+            if not time:
+                time = Time(api_id=jogo['time_fora_id'], nome=jogo['time_fora'])
+                db.session.add(time)
+                db.session.flush()
+            times_cadastrados[jogo['time_fora_id']] = time.id
+        
+        # Cadastra jogo
+        jogo_existente = Jogo.query.filter_by(api_id=jogo['api_id']).first()
+        if not jogo_existente:
+            novo_jogo = Jogo(
+                api_id=jogo['api_id'],
+                rodada=jogo['rodada'],
+                time_casa_id=times_cadastrados[jogo['time_casa_id']],
+                time_fora_id=times_cadastrados[jogo['time_fora_id']],
+                data=jogo['data'],
+                gols_casa=jogo['gols_casa'],
+                gols_fora=jogo['gols_fora']
+            )
+            db.session.add(novo_jogo)
+    
+    db.session.commit()
+    return jsonify({'sucesso': True, 'mensagem': 'Dados importados!', 'jogos': len(jogos)})
